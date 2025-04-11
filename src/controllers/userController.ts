72b5b073faa2ec0c47614dfb399a3d45275aa07f
Nicoli Santos <Nicolisantosbatista@gmail.com>
2025-04-11 17:16:33 -0300

    TypeScript

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -3,7 +3,7 @@ portanto, iremos importar as bibliotecas: request e response do framework expres
 
 import { Request, Response } from "express";
 /** importar a função q traz todos os usuarios */
-import { getAllUsers } from "../models/userModel";
+import { getAllUsers, createUser, updateUser, deleteUser, User } from "../models/userModel";
 
 export async function getUsers(req:Request, res:Response):Promise<void> {
     try{
@@ -13,3 +13,41 @@ export async function getUsers(req:Request, res:Response):Promise<void> {
         res.status(500).json(`Erro ao listas os usuarios -> ${error}`)
     }
 }
+
+// a função create cadastra novos usuarios a partir dos dados enviados pelo frontend. estes dados serão passados via request
+export async function create(req:Request, res:Response):Promise<void>{
+        try{
+        // a constante user guarda o usuario enviado pelo front e passa para o metodo create user
+            const user: Omit<User,"id">=req.body
+            const rs = await createUser(user);
+            res.status(201).json(`Cadastro realizado -> ${rs}`);
+        }
+        catch(err){
+            res.status(500).json(`Erro ao tentar cadastrar ${err}`);
+        }
+    }
+
+    //
+    export async function update(req:Request, res:Response):Promise<void>{
+            try{
+                const user: Omit<User,"id">=req.body
+                const rs = await updateUser(parseInt(req.params.id),user)
+                res.status(201).json(`Atualizado -> ${rs}`);
+            }
+            catch(err){
+                res.status(500).json(`Erro ao tentar cadastrar ${err}`);
+            }
+        }
+
+    //
+    export async function deleta(req:Request, res:Response):Promise<void>{
+            try{
+                const rs = await deleteUser(parseInt(req.params.id))
+                res.status(201).json(`Atualizado -> ${rs}`);
+            }
+            catch(err){
+                res.status(500).json(`Erro ao tentar cadastrar ${err}`);
+            }
+        }
+        
+        
--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,6 +1,8 @@
 // importa a biblioteca do row(linha) data (dados) packet (pacote)
-//guarda todos os dados q retorna da consulta select
-import { RowDataPacket } from "mysql2";
+//guarda todos os dados q retorna da consulta 
+// o result é utilizado para executar as consultas de modificação das tabelas
+// insert | update | delete
+import { RowDataPacket , ResultSetHeader } from "mysql2";
 // importando a conexão c a database p fazer uma consulta nas tabelas do banco.
 import pool from "../database";
 
@@ -19,3 +21,49 @@ export async function getAllUsers():Promise<User[]>{
     const [rows] = await pool.query<User[]>("select * from users", []);
         return rows;
 }
+
+// Função para criar um novo usuário
+// aguarda o usuario ser cadastrado portanto estamos usando a função como async.. await
+// para cadastra um usuario será necessario passar o usuario por parametro e,ele será gerenciado pelo seu id
+
+export async function createUser(user: Omit<User, 'id'>): Promise<ResultSetHeader> {
+        try {
+// usar comando insert p cadastrar o usuario na database. comando await  irá esperar pelo cadastro completo do usuario
+// na consulta do insert está sendo passada 2 parametros c simbolo de "?". consultas parametrizadas evitam a injeção d sql
+          const [result] = await pool.execute<ResultSetHeader>(
+            'INSERT INTO users (name, email) VALUES (?, ?)',
+            [user.name, user.email]
+          );
+          return result;
+        } catch (error) {
+          console.error('Erro ao criar usuário:', error);
+          throw error;
+        }
+      }
+
+
+    // Função para atualizar um usuário existente
+export async function updateUser(id: number, user: Omit<User, 'id'>): Promise<ResultSetHeader> {
+        try {
+          const [result] = await pool.execute<ResultSetHeader>(
+            'UPDATE users SET name = ?, email = ? WHERE id = ?',
+            [user.name, user.email, id]
+          );
+          return result;
+        } catch (error) {
+          console.error('Erro ao atualizar usuário:', error);
+          throw error;
+        }
+      }
+    
+ // Função para deletar um usuário
+  export async function deleteUser(id: number): Promise<ResultSetHeader> {
+    try {
+      const [result] = await pool.execute<ResultSetHeader>('DELETE FROM users WHERE id = ?', [id]);
+      return result;
+    } catch (error) {
+      console.error('Erro ao deletar usuário:', error);
+      throw error;
+    }
+  }
+
